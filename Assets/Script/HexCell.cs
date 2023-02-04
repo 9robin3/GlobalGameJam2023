b8d998cb13ef8914b9c9b8d71153853cac85978c
SweetBlitz <wimmer.wimman@hotmail.se>
2023-02-04 08:24:19 +0100

    HexCell prefab

--- Assets/Script/HexCell.cs
+++ Assets/Script/HexCell.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
 public class HexCell : MonoBehaviour
 {
     public HexSide[] hexSides;
